2064ef1533a3018532ff32cdad4a34751d118bfb
Raphael Moreto <raphaelqmoreto@gmail.com>
2024-09-07 15:07:20 -0300

    commit inicial

---
+++ jogo-xadrez/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace jogo_xadrez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

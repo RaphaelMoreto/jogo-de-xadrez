c8b7637aa882be4d1c402728a3df0ea43a686b46
Raphael Moreto <raphaelqmoreto@gmail.com>
2024-09-07 15:46:38 -0300

    implementado o tabuleiro

--- jogo-xadrez/Program.cs
+++ jogo-xadrez/Program.cs
@@ -9,7 +9,7 @@ namespace jogo_xadrez
         {
             Posicao P = new Posicao(3, 4);
 
-            Console.WriteLine(P);
+            Console.WriteLine("POSIÇÃO: "+ P);
         }
     }
 }
---
+++ jogo-xadrez/tabuleiro/Peca.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qtdMovimentos { get; protected set; }
+        public Tabuleiro tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        {
+            this.posicao = posicao;
+            this.cor = cor;
+            this.tabuleiro = tab;
+            this.qtdMovimentos = 0;
+        }
+    }
+}

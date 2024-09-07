149973da585197cc4cc394d51f0c5565ba7d506e
Raphael Moreto <raphaelqmoreto@gmail.com>
2024-09-07 15:27:08 -0300

    criada classe Posicao

--- jogo-xadrez/Program.cs
+++ jogo-xadrez/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace jogo_xadrez
+﻿using System;
+using tabuleiro;
+
+namespace jogo_xadrez
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Posicao P = new Posicao(3, 4);
+
+            Console.WriteLine(P);
         }
     }
 }
